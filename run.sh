57ec85fbb2b042ee53b99bf673f956966089a4fa
Victor Manuel Gonzalez Opazo <Victor.Opazo@tsoftglobal.com>
2019-11-05 07:31:02 -0300

    Fix port run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker rm -f poc-python-cmpc
-docker run --name poc-python-cmpc -itd -p 80:7000 tsoftglobal/cmpc:poc-python
+docker run --name poc-python-cmpc -itd -p 80:5000 tsoftglobal/cmpc:poc-python
 docker logs -f poc-python-cmpc
